4fd42eb52884ff67cfa5d4648bc8671f0293f661
Andrii <avoevodin81@gmail.com>
2022-09-23 20:14:13 +0300

    pom

---
+++ src/test/java/ua/ithillel/testng/FirstTest.java
@@ -0,0 +1,12 @@
+package ua.ithillel.testng;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void testOne() {
+        Assert.assertTrue(true);
+    }
+}
